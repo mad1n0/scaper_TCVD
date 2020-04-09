607dda0b2ab5bbd9b5b895de8c66ca8ad76462ac
sacamar2 <sacamar2@gmail.com>
2020-04-09 23:02:16 +0200

    Fase 2: Parece que va

--- .idea/main.py
+++ .idea/main.py
@@ -1,17 +1,39 @@
-from scraper import BetScraper
+import requests
+from dateutil import parser
 import datetime
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+import requests
+import numpy as np
 import time
-output_file = "dataset.csv"
-timetosleep=1
-print('init scraper')
-scraper = BetScraper()
-documentnumber=0
-start = datetime.datetime.now()
-while datetime.datetime.now() <= (start + datetime.timedelta(seconds=5)):
-    scraper.scrape()
-    scraper.data2csv(documentnumber)
-    documentnumber=documentnumber+1
-    time.sleep(timetosleep)
+#from scraper import BetScraper
+from scraper_hltv_cuotas import ScraperHltvOdds
 
-scraper.mergedfs()
-scraper.scrape_realtimescore()
+output_file = "dataset.csv"
+
+url="https://www.hltv.org/betting/money"
+
+scraper=ScraperHltvOdds(url=url)
+scraper.scraping_bets()
+print(pd.DataFrame(scraper.data).head())
+
+pd.DataFrame(scraper.data).to_csv(output_file,index=False)
+
+
+#timetosleep=1
+#print('init scraper')
+#scraper = BetScraper()
+#documentnumber=0
+#start = datetime.datetime.now()
+#while datetime.datetime.now() <= (start + datetime.timedelta(seconds=5)):
+#    scraper.scrape()
+#    scraper.data2csv(documentnumber)
+#    documentnumber=documentnumber+1
+#    time.sleep(timetosleep)
+#
+#scraper.mergedfs()
+#scraper.scrape_realtimescore()
+#
+#
+#
+#"https://www.hltv.org/betting/money", "https://www.hltv.org/live"
